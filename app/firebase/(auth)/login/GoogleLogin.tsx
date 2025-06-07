aa249153e4bd9cffa5a158a7a46060e3747a009c
mkhotamirais <mkhotamirais@gmail.com>
2025-06-07 15:16:05 +0700

    kesalahan ismounted

--- app/firebase/(auth)/login/GoogleLogin.tsx
+++ app/firebase/(auth)/login/GoogleLogin.tsx
@@ -1,40 +1,43 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { auth, githubProvider, googleProvider } from "@/config/firebase";
-import { FirebaseError } from "firebase/app";
-import { fetchSignInMethodsForEmail, GoogleAuthProvider, linkWithCredential, signInWithPopup } from "firebase/auth";
+// import { auth, githubProvider, googleProvider } from "@/config/firebase";
+import { auth, googleProvider } from "@/config/firebase";
+// import { FirebaseError } from "firebase/app";
+// import { fetchSignInMethodsForEmail, GoogleAuthProvider, linkWithCredential, signInWithPopup } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 import React from "react";
 import { FaGoogle } from "react-icons/fa6";
-import { toast } from "sonner";
+// import { toast } from "sonner";
 
 export default function GoogleLogin() {
   const onGoogleAuth = async () => {
-    try {
-      await signInWithPopup(auth, googleProvider);
-      toast.success("Berhasil login dengan Google");
-    } catch (err) {
-      const error = err as FirebaseError;
+    await signInWithPopup(auth, googleProvider);
+    // try {
+    //   await signInWithPopup(auth, googleProvider);
+    //   toast.success("Berhasil login dengan Google");
+    // } catch (err) {
+    //   const error = err as FirebaseError;
 
-      if (error.code === "auth/account-exists-with-different-credential") {
-        const pendingCred = GoogleAuthProvider.credentialFromError(error);
-        const email = error.customData?.email;
+    //   if (error.code === "auth/account-exists-with-different-credential") {
+    //     const pendingCred = GoogleAuthProvider.credentialFromError(error);
+    //     const email = error.customData?.email;
 
-        if (email) {
-          const providers = await fetchSignInMethodsForEmail(auth, email as string);
-          if (providers.includes("github.com")) {
-            const result = await signInWithPopup(auth, githubProvider);
-            if (pendingCred) {
-              await linkWithCredential(result.user, pendingCred);
-              toast.success("Akun Google berhasil di-*link* dengan akun GitHub");
-            }
-          }
-        }
-      } else {
-        console.error(error);
-        toast.error(error.message);
-      }
-    }
+    //     if (email) {
+    //       const providers = await fetchSignInMethodsForEmail(auth, email as string);
+    //       if (providers.includes("github.com")) {
+    //         const result = await signInWithPopup(auth, githubProvider);
+    //         if (pendingCred) {
+    //           await linkWithCredential(result.user, pendingCred);
+    //           toast.success("Akun Google berhasil di-*link* dengan akun GitHub");
+    //         }
+    //       }
+    //     }
+    //   } else {
+    //     console.error(error);
+    //     toast.error(error.message);
+    //   }
+    // }
   };
 
   return (
--- app/firebase/(protected)/layout.tsx
+++ app/firebase/(protected)/layout.tsx
@@ -2,12 +2,14 @@
 
 import Loading from "@/components/Loading";
 import { useFirebaseStore } from "@/lib/firebase/store";
+import { useGlobalStore } from "@/lib/globalStore";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const { user, isMounted } = useFirebaseStore();
+  const { user } = useFirebaseStore();
+  const { isMounted } = useGlobalStore();
 
   useEffect(() => {
     if (isMounted && !user) {
