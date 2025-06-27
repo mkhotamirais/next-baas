39188568dcb020552cbbcadc084e6f903f43e70e
mkhotamirais <mkhotamirais@gmail.com>
2025-06-27 13:25:23 +0700

    about

--- app/supabase/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-  return (
-    <section className="section">
-      <div className="container">
-        <h1 className="h1">Supabase About</h1>
-      </div>
-    </section>
-  );
-}
